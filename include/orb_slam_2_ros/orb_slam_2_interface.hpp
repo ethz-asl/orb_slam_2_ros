ace51ebd9906dc24ad883c7fdf30ca8f542e0964
Alex Millane <alex.millane@gmail.com>
2016-12-22 17:59:51 +0100

    Output transform stamped message.

--- include/orb_slam_2_ros/orb_slam_2_interface.hpp
+++ include/orb_slam_2_ros/orb_slam_2_interface.hpp
@@ -4,14 +4,30 @@
 #include <memory>
 #include <string>
 
+#include <geometry_msgs/TransformStamped.h>
+#include <kindr/minimal/quat-transformation.h>
+#include <kindr/minimal/rotation-quaternion.h>
 #include <orb_slam_2/System.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+#include <Eigen/Geometry>
 
 namespace orb_slam_2_interface {
 
+// Convenience typedef
+typedef kindr::minimal::QuatTransformation Transformation;
+typedef kindr::minimal::RotationQuaternion Quaternion;
+
+// NOT NEEDED FOR NOW.
+/*// Stamped transform type
+struct TransformationStamped {
+  ros::Time stamp;
+  Transformation transformation;
+};
+*/
 // Default values for parameters
 static const bool kDefaultVerbose = true;
+static const std::string kDefaultChildFrameName = "world";
 
 // Class handling global alignment calculation and publishing
 class OrbSlam2Interface {
@@ -29,12 +45,25 @@ class OrbSlam2Interface {
   // Callbacks
   void imageCallback(const sensor_msgs::ImageConstPtr& msg);
 
+  // Publishing functions
+  void publishCurrentPose(const Transformation& T, const std_msgs::Header& header);
+
+  // Helper functions
+  void convertOrbSlamPoseToKindr(const cv::Mat& T_cv, Transformation* T_kindr);
+
+  // DEBUG
+  // TODO(alexmillane): Remove this in a release.
+  std::string type2str(int type);
+
   // Node handles
   ros::NodeHandle nh_;
   ros::NodeHandle nh_private_;
 
   // Subscribers
   ros::Subscriber image_sub_;
+
+  // Publishers
+  ros::Publisher T_pub_;
 
   // The orb slam system
   std::shared_ptr<ORB_SLAM2::System> slam_system_;
@@ -43,6 +72,10 @@ class OrbSlam2Interface {
   bool verbose_;
   std::string vocabulary_file_path_;
   std::string settings_file_path_;
+
+  // Transform frame names
+  std::string local_frame_name_;
+
 };
 
 }  // namespace orb_slam_2_interface
--- src/library/orb_slam_2_interface.cpp
+++ src/library/orb_slam_2_interface.cpp
@@ -3,12 +3,18 @@
 #include <cv_bridge/cv_bridge.h>
 #include <glog/logging.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/core/eigen.hpp>
+#include <minkindr_conversions/kindr_msg.h>
+#include <minkindr_conversions/kindr_tf.h>
 
 namespace orb_slam_2_interface {
 
 OrbSlam2Interface::OrbSlam2Interface(const ros::NodeHandle& nh,
                                      const ros::NodeHandle& nh_private)
-    : nh_(nh), nh_private_(nh_private), verbose_(kDefaultVerbose) {
+    : nh_(nh),
+      nh_private_(nh_private),
+      verbose_(kDefaultVerbose),
+      local_frame_name_(kDefaultChildFrameName) {
   // Getting data and params
   subscribeToTopics();
   advertiseTopics();
@@ -27,6 +33,7 @@ void OrbSlam2Interface::subscribeToTopics() {
 
 void OrbSlam2Interface::advertiseTopics() {
   // Advertising topics
+  T_pub_ = nh_private_.advertise<geometry_msgs::TransformStamped>("T", 1);
 }
 
 void OrbSlam2Interface::getParametersFromRos() {
@@ -46,8 +53,97 @@ void OrbSlam2Interface::imageCallback(const sensor_msgs::ImageConstPtr& msg) {
     ROS_ERROR("cv_bridge exception: %s", e.what());
     return;
   }
+
+  // NOTE(alexmillane): On my side of the orb slam interface, correct notation
+  // should be enforced wrt to transformations subscripts. TEST THIS AND LABEL
+  // CORRECTLY.
+
   // Handing the image to ORB slam for tracking
-  slam_system_->TrackMonocular(cv_ptr->image, cv_ptr->header.stamp.toSec());
+  cv::Mat T_cv =
+      slam_system_->TrackMonocular(cv_ptr->image, cv_ptr->header.stamp.toSec());
+
+  // If tracking successfull
+  if (!T_cv.empty()) {
+    // Converting to kindr transform and publishing
+    Transformation T_kindr;
+    convertOrbSlamPoseToKindr(T_cv, &T_kindr);
+    publishCurrentPose(T_kindr, msg->header);
+  }
+}
+
+void OrbSlam2Interface::publishCurrentPose(const Transformation& T,
+                                           const std_msgs::Header& header) {
+  // Creating the message
+  geometry_msgs::TransformStamped msg;
+  // Filling out the header
+  msg.header = header;
+  // Setting the child and parent frames
+  msg.child_frame_id = local_frame_name_;
+  // Converting from a minkindr transform to a transform message
+  tf::transformKindrToMsg(T, &msg.transform);
+  // Publishing the current transformation.
+  T_pub_.publish(msg);
+}
+
+void OrbSlam2Interface::convertOrbSlamPoseToKindr(const cv::Mat& T_cv,
+                                                  Transformation* T_kindr) {
+  // Argument checks
+  CHECK_NOTNULL(T_kindr);
+  CHECK_EQ(T_cv.cols, 4);
+  CHECK_EQ(T_cv.rows, 4);
+  // Open CV mat to Eigen matrix (float)
+  Eigen::Matrix4f T_eigen_f;
+  cv::cv2eigen(T_cv, T_eigen_f);
+  // Eigen matrix (float) to Eigen matrix (double)
+  Eigen::Matrix4d T_eigen_d = T_eigen_f.cast<double>();
+  // Extracting and orthonormalizing the rotation matrix
+  Eigen::Matrix3d R_unnormalized = T_eigen_d.block<3, 3>(0, 0);
+  Eigen::AngleAxisd aa(R_unnormalized);
+  Eigen::Matrix3d R = aa.toRotationMatrix();
+  // Constructing the transformation
+  Quaternion q_kindr(R);
+  Eigen::Vector3d t_kindr(T_eigen_d.block<3, 1>(0, 3));
+  *T_kindr = Transformation(q_kindr, t_kindr);
+}
+
+// DEBUG
+std::string OrbSlam2Interface::type2str(int type) {
+  std::string r;
+
+  uchar depth = type & CV_MAT_DEPTH_MASK;
+  uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+  switch (depth) {
+    case CV_8U:
+      r = "8U";
+      break;
+    case CV_8S:
+      r = "8S";
+      break;
+    case CV_16U:
+      r = "16U";
+      break;
+    case CV_16S:
+      r = "16S";
+      break;
+    case CV_32S:
+      r = "32S";
+      break;
+    case CV_32F:
+      r = "32F";
+      break;
+    case CV_64F:
+      r = "64F";
+      break;
+    default:
+      r = "User";
+      break;
+  }
+
+  r += "C";
+  r += (chans + '0');
+
+  return r;
 }
 
 }  // namespace orb_slam_2_interface
